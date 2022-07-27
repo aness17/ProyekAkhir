57f1174b3c16c9e1a5e6472ebc558ca4d70a4578
aness17 <52444655+aness17@users.noreply.github.com>
2022-07-27 21:27:45 +0700

    apdet

--- application/views/user/dinas_pariwisata.php
+++ application/views/user/dinas_pariwisata.php
@@ -4,12 +4,7 @@
         <div class="container">
 
             <div class="d-flex justify-content-between align-items-center">
-                <h2>Blog Details</h2>
-                <ol>
-                    <li><a href="index.html">Home</a></li>
-                    <li><a href="blog.html">Blog</a></li>
-                    <li>Blog Details</li>
-                </ol>
+                <h2>Berita Viera Oleh-oleh</h2>
             </div>
 
         </div>
@@ -29,54 +24,62 @@
                             <img src="assets/img/blog/blog-1.jpg" alt="" class="img-fluid">
                         </div>
 
-                        <h2 class="title">Dolorum optio tempore voluptas dignissimos cumque fuga qui quibusdam quia</h2>
+                        <h2 class="title">Viera Oleh-oleh Bekerjasama dengan Dinas Pariwisata dan Kebudayaan Kota Pekanbaru</h2>
 
                         <div class="meta-top">
                             <ul>
-                                <li class="d-flex align-items-center"><i class="bi bi-person"></i> <a href="blog-details.html">John Doe</a></li>
-                                <li class="d-flex align-items-center"><i class="bi bi-clock"></i> <a href="blog-details.html"><time datetime="2020-01-01">Jan 1, 2022</time></a></li>
+                                <li class="d-flex align-items-center"><i class="bi bi-person"></i> <a href="https://www.pekanbaru.go.id/p/news/wawako-resmikan-nama-baru-rumah-kue-viera-jadi-viera-oleh-oleh">Berita Pemko</a></li>
+                                <li class="d-flex align-items-center"><i class="bi bi-clock"></i> <a href="blog-details.html"><time datetime="2020-01-01">Des 18, 2021</time></a></li>
                             </ul>
                         </div><!-- End meta top -->
 
                         <div class="content">
                             <p>
-                                Similique neque nam consequuntur ad non maxime aliquam quas. Quibusdam animi praesentium. Aliquam et laboriosam eius aut nostrum quidem aliquid dicta.
-                                Et eveniet enim. Qui velit est ea dolorem doloremque deleniti aperiam unde soluta. Est cum et quod quos aut ut et sit sunt. Voluptate porro consequatur assumenda perferendis dolore.
+                                Wakil Walikota (Wawako) Pekanbaru H. Ayat Cahyadi, meresmikan perubahan nama atau rebranding Rumah Kue Viera menjadi Viera Oleh-oleh, Sabtu (18/12).
+                            </p>
+                            <p>
+                                Pada kegiatan yang dipusatkan di toko utama Viera Oleh-oleh di Jalan Melati ini, turut juga dilakukan Grand Opening satu unit bisnis baru yang dinamakan Viera Souvenir.
                             </p>
 
                             <p>
-                                Sit repellat hic cupiditate hic ut nemo. Quis nihil sunt non reiciendis. Sequi in accusamus harum vel aspernatur. Excepturi numquam nihil cumque odio. Et voluptate cupiditate.
-                            </p>
-
-                            <blockquote>
-                                <p>
-                                    Et vero doloremque tempore voluptatem ratione vel aut. Deleniti sunt animi aut. Aut eos aliquam doloribus minus autem quos.
-                                </p>
-                            </blockquote>
+                                "Kami atas nama Pemerintah Kota Pekanbaru menyambut baik serta mengucapkan selamat atas grand opening bisnis baru Viera Oleh-oleh sebagai suatu bentuk peran dunia usaha dalam pemberdayaan ekonomi
+                                kerakyatan dan meningkatkan perekonomian daerah serta kesejahteraan masyarakat Kota Pekanbaru," ucap Wawako dalam sambutannya.</p>
 
                             <p>
-                                Sed quo laboriosam qui architecto. Occaecati repellendus omnis dicta inventore tempore provident voluptas mollitia aliquid. Id repellendus quia. Asperiores nihil magni dicta est suscipit perspiciatis. Voluptate ex rerum assumenda dolores nihil quaerat.
-                                Dolor porro tempora et quibusdam voluptas. Beatae aut at ad qui tempore corrupti velit quisquam rerum. Omnis dolorum exercitationem harum qui qui blanditiis neque.
-                                Iusto autem itaque. Repudiandae hic quae aspernatur ea neque qui. Architecto voluptatem magni. Vel magnam quod et tempora deleniti error rerum nihil tempora.
-                            </p>
+                                Menurutnya, kehadiran gerai Viera Oleh-oleh merupakan salah satu indikator kemajuan Kota Pekanbaru yang terus berkembang pesat dan telah berkembang menjadi kota dunia usaha.</p>
 
-                            <h3>Et quae iure vel ut odit alias.</h3>
                             <p>
-                                Officiis animi maxime nulla quo et harum eum quis a. Sit hic in qui quos fugit ut rerum atque. Optio provident dolores atque voluptatem rem excepturi molestiae qui. Voluptatem laborum omnis ullam quibusdam perspiciatis nulla nostrum. Voluptatum est libero eum nesciunt aliquid qui.
-                                Quia et suscipit non sequi. Maxime sed odit. Beatae nesciunt nesciunt accusamus quia aut ratione aspernatur dolor. Sint harum eveniet dicta exercitationem minima. Exercitationem omnis asperiores natus aperiam dolor consequatur id ex sed. Quibusdam rerum dolores sint consequatur quidem ea.
-                                Beatae minima sunt libero soluta sapiente in rem assumenda. Et qui odit voluptatem. Cum quibusdam voluptatem voluptatem accusamus mollitia aut atque aut.
-                            </p>
-                            <img src="assets/img/blog/blog-inside-post.jpg" class="img-fluid" alt="">
+                                "Kami juga berbahagia, karena selain menjual produk buatan sendiri, Viera Oleh-oleh juga memasarkan produk-produk UMKM tempatan. Hal ini menjadi nilai tersendiri bagi gerai Viera Oleh-oleh, karena mampu mengangkat usaha masyarakat setempat," ujarnya.</p>
+                            <img src="<?= base_url('assets/') ?>pelanggan/assets/img/dinas.jpeg" alt="" class="img-fluid" width="400px">
 
-                            <h3>Ut repellat blanditiis est dolore sunt dolorum quae.</h3>
                             <p>
-                                Rerum ea est assumenda pariatur quasi et quam. Facilis nam porro amet nostrum. In assumenda quia quae a id praesentium. Quos deleniti libero sed occaecati aut porro autem. Consectetur sed excepturi sint non placeat quia repellat incidunt labore. Autem facilis hic dolorum dolores vel.
-                                Consectetur quasi id et optio praesentium aut asperiores eaque aut. Explicabo omnis quibusdam esse. Ex libero illum iusto totam et ut aut blanditiis. Veritatis numquam ut illum ut a quam vitae.
+                                Dikatakan wawako, saat ini pola pikir masyarakat terus meningkat. Dari dahulunya hanya berada dalam zona nyaman, sekarang sudah berkembang dengan inovasi dan kreativitas. Salah satunya adalah dengan membuka peluang usaha untuk menciptakan lapangan pekerjaan yang berfikir dan bekerja dengan kreativitas yang tinggi, inovasi, serta semangat dan jiwa entrepreneur.
                             </p>
                             <p>
-                                Alias quia non aliquid. Eos et ea velit. Voluptatem maxime enim omnis ipsa voluptas incidunt. Nulla sit eaque mollitia nisi asperiores est veniam.
+                                "Kepada owner Viera Oleh-oleh, saya mengucapkan selamat dan tahniah. Semoga Grand Launching dan Rebranding ini dapat berjaya dan sukses mendapatkan tempat di hati masyarakat Kota Pekanbaru," harapnya.
                             </p>
-
+                            <p>
+                                Sementara itu, wner Viera Oleh-oleh Rahmat mengatakan pihaknya selalu berorientasi untuk mengembangkan bisnis rumah kuenya itu, salah satunya dengan menjadi leading sector di bisnis oleh-oleh.
+                            </p>
+                            <p>
+                                Setelah Rumah Kue Viera (RKV) berhasil menjadi pemimpin pasar untuk oleh-oleh dalam bentuk kue dan yang sejenis, RKV ingin melengkapi kebutuhan wisatawan dan masyarakat untuk produk souvenir khas Riau. </p>
+                            <p>"Kami bertransformasi dari yang dulunya hanya rumah kue atau kuliner, sekarang melebarkan sayap sebagai pusat oleh-oleh khas Riau. Untuk itu, RKV membentuk satu unit bisnis baru yang dinamakan Viera Souvenir dan rebranding menjadi Viera Oleh-oleh," kata Rahmat didampingi istrinya, Evi.
+                            </p>
+                            <p>
+                                Viera Souvenir, kata Rahmat menghadirkan berbagai souvenir khas Riau seperti batik, tenun, songket, tanjak, kaos merchandise, gantungan kunci, topi dan banyak lagi.
+                            </p>
+                            <p>
+                                "Produk yang tersedia di outlet Viera Souvenir diperoleh dari berbagai UMKM seluruh Riau. Dan kerjasama ini juga bermaksud untuk meningkatkan akses pasar bagi para pelaku UMKM Riau," sebutnya.</p>
+                            <p>Ia menegaskan bahwa kerjasama yang dibangun dengan para UMKM yang menjadi mitra Viera Souvenir ini tidak hanya dari sisi penjualan tapi juga pengembangan bisnis.
+                                "Sehingga Viera dan UMKM Riau bisa tumbuh dan berkembang bersama," ungkapnya.
+                            </p>
+                            <p>
+                                Untuk diketahui, RKV didirikan oleh Rahmad dan Evi pada 2015 lalu. Usaha rumahan ini dimulai dari rumah kecil di Perumahan Bidadari Panam. Awalnya mereka menjajakan dan berbagi kue yang mereka buat kepada warga disekitar tempat tinggalnya hingga berkembang seperti saat ini.
+                            </p>
+                            <p>
+                                Rumah Kue Viera memang #berangkatdarirumah tapi tidak berhenti di rumah saja. Maka untuk terus tumbuh, mampu bersaing dan menjadi indentitas merk yang kuat Rumah Kue Viera siap berubah menjadi Viera Oleh-oleh.
+                            </p>
+                            <p>Nama Viera Oleh-oleh sekaligus menjadi penanda bahwa Viera siap mejadi pusat oleh-oleh terlengkap dan terbesar di Riau. Ditambah lagi dengan Tagline #Tandasayangdaririau, Viera ingin memberikan ungkapan kepada setiap pengunjung yang datang dan membawa pulang sebuah tanda sayang untuk orang tersayang yang sedang menunggu di rumah. (Kominfo2/RD1)</p>
                         </div><!-- End post content -->
 
 </main>
--- application/views/user/penghargaan_muri.php
+++ application/views/user/penghargaan_muri.php
@@ -4,12 +4,7 @@
         <div class="container">
 
             <div class="d-flex justify-content-between align-items-center">
-                <h2>Blog Details</h2>
-                <ol>
-                    <li><a href="index.html">Home</a></li>
-                    <li><a href="blog.html">Blog</a></li>
-                    <li>Blog Details</li>
-                </ol>
+                <h2>Berita Viera Oleh-oleh</h2>
             </div>
 
         </div>
@@ -29,53 +24,45 @@
                             <img src="assets/img/blog/blog-1.jpg" alt="" class="img-fluid">
                         </div>
 
-                        <h2 class="title">Dolorum optio tempore voluptas dignissimos cumque fuga qui quibusdam quia</h2>
+                        <h2 class="title">Viera Oleh-oleh Raih Rekor Muri Ketan Talam Durian Terpanjang</h2>
 
                         <div class="meta-top">
                             <ul>
-                                <li class="d-flex align-items-center"><i class="bi bi-person"></i> <a href="blog-details.html">John Doe</a></li>
-                                <li class="d-flex align-items-center"><i class="bi bi-clock"></i> <a href="blog-details.html"><time datetime="2020-01-01">Jan 1, 2022</time></a></li>
+                                <li class="d-flex align-items-center"><i class="bi bi-person"></i> <a href="blog-details.html">Haswandi</a></li>
+                                <li class="d-flex align-items-center"><i class="bi bi-clock"></i> <a href="blog-details.html"><time datetime="2020-01-01">Mei 21, 2022</time></a></li>
                             </ul>
                         </div><!-- End meta top -->
 
                         <div class="content">
                             <p>
-                                Similique neque nam consequuntur ad non maxime aliquam quas. Quibusdam animi praesentium. Aliquam et laboriosam eius aut nostrum quidem aliquid dicta.
-                                Et eveniet enim. Qui velit est ea dolorem doloremque deleniti aperiam unde soluta. Est cum et quod quos aut ut et sit sunt. Voluptate porro consequatur assumenda perferendis dolore.
+                                Viera Oleh-oleh atau yang lebih dikenal dengan Ketan Talam Durian Viera Pekanbaru, raih rekor Museum Rekor Dunia Indonesia (Muri).
+
+                                Viera Oleh-oleh mendapatkan Rekor Muri dengan kategori Rangkaian Kue Talam Durian Terpanjang, yang panjangnya mencapai 9 meter.</p>
+                            <p>
+                                Penyerahan piagam Rekor Muri ini berlangsung di Mal Pekanbaru Xchange (PX) Jalan Riau, Kota Pekanbaru, Jumat 20 Mei 2022.
+                            </p>
+                            <p>
+                                Menurut Owner Viera Oleh-oleh, Rahmad, untuk menyajikan ketan talam durian sepanjang 9 meter itu menghabiskan 70 kilogram ketan.
+
+                                Proses pembuatannya berlangsung selama 6 jam, mulai pukul 05.00 WIB hingga pukul 11.00 WIB.
+
+                                Meski proses pembuatannya butuh waktu panjang, namun hasilnya sangat memuaskan. </p>
+
+                            <p>
+                                Rahmad menceritakan, awalnya Viera Oleh-oleh di ajak berkolaborasi dalam suatu event di PX Pekanbaru.
+
+                                Tak sia-siakan kesempatan, Viera terima tantangan dan tawaran Rekor MURI dengan penyajian ketan talam durian terpanjang.
                             </p>
 
                             <p>
-                                Sit repellat hic cupiditate hic ut nemo. Quis nihil sunt non reiciendis. Sequi in accusamus harum vel aspernatur. Excepturi numquam nihil cumque odio. Et voluptate cupiditate.
+                                Menurut Rahmad, dengan adanya Rekor Muri ini, ia berharap ketan talam durian kian populer dan jadi produk kebanggan Riau.
                             </p>
-
-                            <blockquote>
-                                <p>
-                                    Et vero doloremque tempore voluptatem ratione vel aut. Deleniti sunt animi aut. Aut eos aliquam doloribus minus autem quos.
-                                </p>
-                            </blockquote>
+                            <img src="<?= base_url('assets/') ?>pelanggan/assets/img/murii.jpeg" alt="" class="img-fluid" width="400px">
 
                             <p>
-                                Sed quo laboriosam qui architecto. Occaecati repellendus omnis dicta inventore tempore provident voluptas mollitia aliquid. Id repellendus quia. Asperiores nihil magni dicta est suscipit perspiciatis. Voluptate ex rerum assumenda dolores nihil quaerat.
-                                Dolor porro tempora et quibusdam voluptas. Beatae aut at ad qui tempore corrupti velit quisquam rerum. Omnis dolorum exercitationem harum qui qui blanditiis neque.
-                                Iusto autem itaque. Repudiandae hic quae aspernatur ea neque qui. Architecto voluptatem magni. Vel magnam quod et tempora deleniti error rerum nihil tempora.
+                                “Ini persembahan Viera untuk Pekanbaru dan Riau yang ke-2 setelah Anugerah Pesona Indonesia Makanan Tradisional Terpopuler 2019,” ujar Rahmad.
                             </p>
 
-                            <h3>Et quae iure vel ut odit alias.</h3>
-                            <p>
-                                Officiis animi maxime nulla quo et harum eum quis a. Sit hic in qui quos fugit ut rerum atque. Optio provident dolores atque voluptatem rem excepturi molestiae qui. Voluptatem laborum omnis ullam quibusdam perspiciatis nulla nostrum. Voluptatum est libero eum nesciunt aliquid qui.
-                                Quia et suscipit non sequi. Maxime sed odit. Beatae nesciunt nesciunt accusamus quia aut ratione aspernatur dolor. Sint harum eveniet dicta exercitationem minima. Exercitationem omnis asperiores natus aperiam dolor consequatur id ex sed. Quibusdam rerum dolores sint consequatur quidem ea.
-                                Beatae minima sunt libero soluta sapiente in rem assumenda. Et qui odit voluptatem. Cum quibusdam voluptatem voluptatem accusamus mollitia aut atque aut.
-                            </p>
-                            <img src="assets/img/blog/blog-inside-post.jpg" class="img-fluid" alt="">
-
-                            <h3>Ut repellat blanditiis est dolore sunt dolorum quae.</h3>
-                            <p>
-                                Rerum ea est assumenda pariatur quasi et quam. Facilis nam porro amet nostrum. In assumenda quia quae a id praesentium. Quos deleniti libero sed occaecati aut porro autem. Consectetur sed excepturi sint non placeat quia repellat incidunt labore. Autem facilis hic dolorum dolores vel.
-                                Consectetur quasi id et optio praesentium aut asperiores eaque aut. Explicabo omnis quibusdam esse. Ex libero illum iusto totam et ut aut blanditiis. Veritatis numquam ut illum ut a quam vitae.
-                            </p>
-                            <p>
-                                Alias quia non aliquid. Eos et ea velit. Voluptatem maxime enim omnis ipsa voluptas incidunt. Nulla sit eaque mollitia nisi asperiores est veniam.
-                            </p>
 
                         </div><!-- End post content -->
 
